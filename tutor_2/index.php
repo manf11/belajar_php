5fcb6066c827446a3aac1cd50ac67f3bc393eb5b
manf11 <mohahlisnoorfuad1@gmail.com>
2021-01-13 08:56:20 +0700

    belajar php tutor ke 2 wpu

---
+++ tutor_2/index.php
@@ -0,0 +1,36 @@
+<?php
+// komentar dalam satu baris
+/*untuk banyak baris*/
+
+// tutor 2
+// sintaks dasar
+// // standar output
+// echo,print
+// print_r and var_dump() = buat debugging
+// var_dump ("ahlis");
+// operator
+// aritmatika = + - * /%
+
+// penggabung string / concatenation / concat = .
+// $nama_depan = "ahlis";
+// $nama_belakang = "nor";
+// operator assigmen adalah penugasan =,+=,-=,*=,/=,%=,.=
+// perbandingan <,>,<=,>=,==,!=
+// var_dump(1>5);
+// identitas ===,!==
+// var_dump(1 === "1");
+// logika &&,||,!
+$x=10;
+var_dump($x<20 && $x %2 == 0);
+?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar php</title>
+</head>
+<body>
+    <h1> selamt datang <?php echo $nama_depan . " " . $nama_belakang;?></h1>
+</body>
+</html> -->
